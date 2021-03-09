16d3e8198cbb0065cb3be6c6693c4073ab7bada1
veronica <andreia.mp.ferreira@gmail.com>
2021-03-09 23:13:09 +0100

    update buttons actions and add eel

--- app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
@@ -3,42 +3,67 @@ package com.example.androiddevchallenge
 import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainViewModel: ViewModel() {
     private val _time = MutableLiveData<Long>(0)
-    val time: LiveData<Long> = _time
+    val time: LiveData<String> = Transformations.map(_time) { timeLiveData ->
+        val timeFraction = BigDecimal(timeLiveData).divide(BigDecimal(1000)).setScale(2, RoundingMode.UP)
+        if (timeFraction < BigDecimal.TEN) {
+            "0$timeFraction"
+        } else {
+            "$timeFraction"
+        }
+    }
 
-    private val _isStartEnabled = MutableLiveData(true)
-    val isStartEnabled: LiveData<Boolean> = _isStartEnabled
+    private val _isTimerStartEnabled = MutableLiveData(false)
+    val isTimerStartEnabled: LiveData<Boolean> = _isTimerStartEnabled
+
+    private val _isTimerControllerEnabled = MutableLiveData(true)
+    val isTimerControllerEnabled: LiveData<Boolean> = _isTimerControllerEnabled
+
+    private val _isEelVisible = MutableLiveData(false)
+    val isEelVisible: LiveData<Boolean> = _isTimerControllerEnabled
 
     fun startTimer() {
-        val howLong = _time.value ?: 0
+        _isTimerStartEnabled.value = false
+        _isTimerControllerEnabled.value = false
+        val howLong = (_time.value ?: 0)
         object : CountDownTimer(
             howLong,
             1
         ) {
             override fun onTick(millisUntilFinished: Long) {
-                _isStartEnabled.value = false
                 _time.value = millisUntilFinished
             }
 
             override fun onFinish() {
                 _time.value = 0
+                _isTimerControllerEnabled.value = true
+                _isEelVisible.value = true
             }
         }.start()
     }
 
     fun addTime() {
         _time.value = (_time.value ?: 0) + 1000
-        _isStartEnabled.value = true
+        _isTimerStartEnabled.value = true
+        _isTimerControllerEnabled.value = true
+        _isEelVisible.value = false
     }
 
     fun decrementTime() {
         _time.value?.let {
-            if (it - 1000 > 0) {
+            if (it > 0) {
                 _time.value = it - 1000
             }
         }
+    }
+
+    private fun timerFormat(timeMillis: Int) {
+        val formattedTime = BigDecimal(timeMillis/1000).setScale(2)
     }
 }
--- app/src/main/java/com/example/androiddevchallenge/view/MainTimerView.kt
+++ app/src/main/java/com/example/androiddevchallenge/view/MainTimerView.kt
@@ -1,6 +1,6 @@
 package com.example.androiddevchallenge.view
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -12,14 +12,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.androiddevchallenge.MainViewModel
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import com.example.androiddevchallenge.R
+import java.math.BigDecimal
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
@@ -40,8 +41,10 @@ fun DarkPreview() {
 // Start building your app here!
 @Composable
 fun MyApp(mainViewModel: MainViewModel = viewModel()) {
-    val time by mainViewModel.time.observeAsState(0)
-    val isButtonEnabled by mainViewModel.isStartEnabled.observeAsState(true)
+    val time by mainViewModel.time.observeAsState("00.00")
+    val isTimerButtonEnabled by mainViewModel.isTimerStartEnabled.observeAsState(false)
+    val isTimerControllerEnabled by mainViewModel.isTimerControllerEnabled.observeAsState(true)
+    val isEelVisible by mainViewModel.isEelVisible.observeAsState(false)
 
     Surface(color = MaterialTheme.colors.background) {
         Column(
@@ -50,26 +53,40 @@ fun MyApp(mainViewModel: MainViewModel = viewModel()) {
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            TimerComponent(time = time, onAdd = { mainViewModel.addTime() }, onDecrease = { mainViewModel.decrementTime() })
-            Text(
-                text = "Ready... Set...",
-                fontSize = 20.sp
-            )
-            Button(
-                enabled = isButtonEnabled,
-                onClick = {
-                    mainViewModel.startTimer()
+            //if (isEelVisible) {
+              //  EelComponent()
+            //} else {
+                TimerComponent(
+                    time = time,
+                    isTimerControllerEnabled = isTimerControllerEnabled,
+                    onAdd = { mainViewModel.addTime() },
+                    onDecrease = { mainViewModel.decrementTime() }
+                )
+                Text(
+                    text = "Ready... Set...",
+                    fontSize = 20.sp
+                )
+                Button(
+                    enabled = isTimerButtonEnabled,
+                    onClick = {
+                        mainViewModel.startTimer()
+                    }
+                ) {
+                    Text(text = "EEl!")
                 }
-            ) {
-                Text(text = "EEl!")
             }
-        }
+       // }
 
     }
 }
 
 @Composable
-fun TimerComponent(time: Long, onAdd: (Unit) -> Unit, onDecrease: (Unit) -> Unit) {
+fun TimerComponent(
+    time: String,
+    isTimerControllerEnabled: Boolean,
+    onAdd: (Unit) -> Unit,
+    onDecrease: (Unit) -> Unit
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -77,19 +94,34 @@ fun TimerComponent(time: Long, onAdd: (Unit) -> Unit, onDecrease: (Unit) -> Unit
             text = "-",
             fontSize = 52.sp,
             modifier = Modifier
-                .clickable(onClick = { onDecrease.invoke(Unit) })
+                .clickable(
+                    enabled = isTimerControllerEnabled,
+                    onClick = { onDecrease.invoke(Unit) }
+                )
                 .padding(16.dp)
         )
         Text(
-            text = "$time",
+            text = time,
             fontSize = 52.sp
         )
         Text(
             text = "+",
             fontSize = 52.sp,
             modifier = Modifier
-                .clickable(onClick = { onAdd.invoke(Unit) })
+                .clickable(
+                    enabled = isTimerControllerEnabled,
+                    onClick = { onAdd.invoke(Unit) }
+                )
                 .padding(16.dp)
         )
+    }
+}
+
+@Composable
+fun EelComponent() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(painter = painterResource(id = R.drawable.eel_o_clock), contentDescription = null)
     }
 }
